f8b5161dfd1d5d78aba877f64dc044a48cb81fb7
ayu2000y15 <ayu2000y15@nasystem.net>
2024-11-20 11:12:45 +0900

    　　日付：2024/11/20 　変更者：野口 変更内容：タレント経歴更新バグ修正

--- app/Http/Controllers/Admin/TalentAdminController.php
+++ app/Http/Controllers/Admin/TalentAdminController.php
@@ -372,11 +372,11 @@ class TalentAdminController extends Controller
         $validatedData = $request->validate([
             'CAREER_CATEGORY_ID' => 'required|exists:career_categories,CAREER_CATEGORY_ID',
             'CONTENT' => 'required|string',
-            'ACTIVE_DATE' => 'required|date',
+            'ACTIVE_DATE' => 'nullable|date',
             'DETAIL' => 'nullable|string',
         ]);
 
-        $career = TalentCareer::where('TALENT_ID', $request->TALENT_ID);
+        $career = TalentCareer::where('CAREER_ID', $request->CAREER_ID);
         $career->update($validatedData);
 
         session()->flash('activeTabT', 'talent-career');
